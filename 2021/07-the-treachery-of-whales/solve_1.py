f97969088892470ff093fe44884c1dde73f681a7
Carlos Perez Alcaraz <alcacarl@amazon.com>
2021-12-07 14:23:51 +0100

    Add solution for puzzle 07 of 2021

---
+++ 2021/07-the-treachery-of-whales/solve_1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def getMinFuelToAlignPositions(positions):
+	fuelNeeded = lambda distance: distance
+
+	return min(map(
+		lambda target: sum(fuelNeeded(abs(position - target)) for position in positions),
+		range(min(positions), max(positions) + 1)
+	))
+
+
+if __name__ == '__main__':
+	positions = list(map(int, input().split(',')))
+	print(getMinFuelToAlignPositions(positions))
