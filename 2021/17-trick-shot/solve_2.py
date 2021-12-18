819cbaeb3413de2e32283536d28ca743e247759d
Carlos Perez Alcaraz <alcacarl@amazon.com>
2021-12-18 14:51:33 +0100

    Add solution for puzzle 17 of 2021

---
+++ 2021/17-trick-shot/solve_2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import collections
+import re
+
+MAX_STEPS = 160
+
+def getPossibleVelocitiesToReachTarget(minX, maxX, minY, maxY):
+	possibleVxBySteps = collections.defaultdict(list)
+	possibleVelocities = set()
+
+	for vx in range(maxX + 1):
+		x, dx = 0, vx
+
+		for step in range(1, MAX_STEPS):
+			x += dx
+			dx = max(0, dx - 1)
+
+			if minX <= x <= maxX:
+				possibleVxBySteps[step].append(vx)
+
+	for vy in range(minY, max(-minY, maxY)):
+		y, dy = 0, vy
+
+		for step in range(1, MAX_STEPS):
+			y += dy
+			dy -= 1
+
+			if minY <= y <= maxY:
+				possibleVelocities.update((vx, vy) for vx in possibleVxBySteps[step])
+
+	return possibleVelocities
+
+def countPossibleVelocitiesToReachTarget(*args):
+	return len(getPossibleVelocitiesToReachTarget(*args))
+
+
+if __name__ == '__main__':
+	numbers = [int(digits) for digits in re.compile(r'-?\d+').findall(input())]
+
+	print(countPossibleVelocitiesToReachTarget(*numbers))
