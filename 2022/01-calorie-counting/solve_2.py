30de2a295133a5242513e794d4ed7bed9251b1fd
Carlos Perez Alcaraz <alcacarl@amazon.com>
2022-12-01 23:47:44 +0100

    Add solution for puzzle 01 of 2022

---
+++ 2022/01-calorie-counting/solve_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def solve(caloriesByElf, count):
+	sortedSumOfCaloriesByElf = sorted(map(sum, caloriesByElf), reverse=True)
+
+	return sum(sortedSumOfCaloriesByElf[:count])
+
+
+if __name__ == '__main__':
+	caloriesByElf = [
+		list(map(int, lines.splitlines()))
+		for lines in sys.stdin.read().split('\n' * 2)
+	]
+
+	print(solve(caloriesByElf, 3))
