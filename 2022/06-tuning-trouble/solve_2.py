69cbfed13bf022173818ea8da2ebdc825e2fe412
Carlos Perez Alcaraz <alcacarl@amazon.com>
2022-12-06 22:00:53 +0100

    Add solution for puzzle 06 of 2022

---
+++ 2022/06-tuning-trouble/solve_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+WINDOW_SIZE = 14
+
+def getPositionOfFirstStartOfMessageMarker(signal):
+	for idx in range(len(signal)):
+		if idx >= WINDOW_SIZE - 1 and len(set(signal[idx - WINDOW_SIZE + 1:idx + 1])) == WINDOW_SIZE:
+			return idx + 1
+
+
+if __name__ == '__main__':
+	signal = input()
+
+	print(getPositionOfFirstStartOfMessageMarker(signal))
