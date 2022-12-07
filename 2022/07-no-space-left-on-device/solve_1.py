22aa5b79296a80101d833b9663fa267e834a023e
Carlos Perez Alcaraz <alcacarl@amazon.com>
2022-12-07 21:50:45 +0100

    Add solution for puzzle 07 of 2022

---
+++ 2022/07-no-space-left-on-device/solve_1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def parseTerminalOutput(terminalOutput):
+	path = [{}]
+
+	for line in terminalOutput:
+		match line.split():
+			case [_, 'cd', '/']:    del path[1:]
+			case [_, 'cd', '..']:   path.pop()
+			case [_, 'cd', dir]:    path += [path[-1].get(dir, {})]
+			case ['$', 'ls']:       pass
+			case ['dir', name]:     path[-1][name] = {}
+			case [size, name]:      path[-1][name] = int(size)
+
+	return path[0]
+
+def computeDirectorySizes(root):
+	result = []
+	size = 0
+
+	for item in root.values():
+		if type(item) is dict:
+			result += computeDirectorySizes(item)
+			size += result[-1]
+		else:
+			size += item
+
+	return result + [size]
+
+def solve(terminalOutput):
+	filesystemRoot = parseTerminalOutput(terminalOutput)
+	directorySizes = computeDirectorySizes(filesystemRoot)
+
+	return sum(size for size in directorySizes if size <= 100000)
+
+
+if __name__ == '__main__':
+	terminalOutput = sys.stdin.readlines()
+
+	print(solve(terminalOutput))
