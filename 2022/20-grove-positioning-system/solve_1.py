fb933e9d72a60016b4ea8697fadd9607f6978428
Carlos Perez Alcaraz <alcacarl@amazon.com>
2022-12-20 22:21:44 +0100

    Add solution for puzzle 20 of 2022

---
+++ 2022/20-grove-positioning-system/solve_1.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+MIX_ROUNDS = 1
+DECRYPTION_KEY = 1
+
+def decrypt(encryptedFile):
+	numbers = [number * DECRYPTION_KEY for number in encryptedFile]
+	indices = list(range(len(numbers)))
+
+	for _ in range(MIX_ROUNDS):
+		for idx, number in enumerate(numbers):
+			currentPosition = indices.index(idx)
+			newPosition = (currentPosition + number) % (len(numbers) - 1)
+			indices.pop(currentPosition)
+			indices.insert(newPosition, idx)
+
+	return [numbers[idx] for idx in indices]
+
+def getGroveCoordinates(encryptedFile):
+	decryptedFile = decrypt(encryptedFile)
+	zeroIndex = decryptedFile.index(0)
+
+	return sum(decryptedFile[(zeroIndex + delta) % len(encryptedFile)] for delta in [1000, 2000, 3000])
+
+
+if __name__ == '__main__':
+	encryptedFile = list(map(int, sys.stdin.readlines()))
+
+	print(getGroveCoordinates(encryptedFile))
