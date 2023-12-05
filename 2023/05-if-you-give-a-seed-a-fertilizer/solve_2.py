01b46a2b489ecea1e1b4943f35610c5dade72d12
Carlos Perez Alcaraz <alcacarl@amazon.com>
2023-12-05 22:18:18 +0100

    Add solution for puzzle 05 of 2023

---
+++ 2023/05-if-you-give-a-seed-a-fertilizer/solve_2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def applyMappings(seedRanges, maps):
+	for map in maps:
+		mappedRanges = []
+
+		for mapStart, mapEnd, mapOffset in map:
+			unmappedRanges = []
+
+			for seedStart, seedEnd in seedRanges:
+				if mapStart > seedEnd or mapEnd < seedStart:
+					unmappedRanges += [(seedStart, seedEnd)]
+				else:
+					intersectionStart, intersectionEnd = max(seedStart, mapStart), min(seedEnd, mapEnd)
+
+					mappedRanges += [(intersectionStart + mapOffset, intersectionEnd + mapOffset)]
+					if seedStart < intersectionStart: unmappedRanges += [(seedStart, intersectionStart - 1)]
+					if seedEnd > intersectionEnd: unmappedRanges += [(intersectionEnd + 1, seedEnd)]
+
+			seedRanges = unmappedRanges
+
+		seedRanges.extend(mappedRanges)
+
+	return seedRanges
+
+def parseMap(lines):
+	return [
+		(sourceRangeStart, sourceRangeStart + rangeLength - 1, destinationRangeStart - sourceRangeStart)
+		for destinationRangeStart, sourceRangeStart, rangeLength in (map(int, line.split()) for line in lines.splitlines()[1:])
+	]
+
+def solve(almanac):
+	seeds = map(int, almanac[0][7:].split(' '))
+	maps = map(parseMap, almanac[1:])
+	seedRanges = [(start, start + length - 1) for start, length in zip(*(iter(seeds),) * 2)]
+
+	return min(start for start, _ in applyMappings(seedRanges, maps))
+
+
+if __name__ == '__main__':
+	almanac = sys.stdin.read().split('\n\n')
+
+	print(solve(almanac))
