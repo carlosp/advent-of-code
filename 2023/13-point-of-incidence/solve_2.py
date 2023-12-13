0a54302f756654f02435c55d84ca4ef5a1974c6d
Carlos Perez Alcaraz <alcacarl@amazon.com>
2023-12-13 20:28:50 +0100

    Add solution for puzzle 13 of 2023

---
+++ 2023/13-point-of-incidence/solve_2.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+def summarizeReflection(lines, line):
+	before, after = line, line + 1
+
+	while before >= 0 and after < len(lines):
+		if lines[before] != lines[after]:
+			return 0
+
+		before -= 1
+		after += 1
+
+	return line + 1
+
+def summarizePattern(rows, cols, exclude=None):
+	for lines, multiplier in [(rows, 100), (cols, 1)]:
+		for line in range(len(lines) - 1):
+			if (summary := multiplier * summarizeReflection(lines, line)) and summary != exclude:
+				return summary
+
+def solve(pattern):
+	height, width = len(pattern), len(pattern[0])
+	rows = [[row[col] == '.' for col in range(width)] for row in pattern]
+	cols = [[row[col] == '.' for row in pattern] for col in range(width)]
+	brokenSummary = summarizePattern(rows, cols)
+
+	for row in range(height):
+		for col in range(width):
+			rows[row][col] = cols[col][row] = not rows[row][col]
+
+			if fixedSummary := summarizePattern(rows, cols, brokenSummary):
+				return fixedSummary
+
+			rows[row][col] = cols[col][row] = not rows[row][col]
+
+
+if __name__ == '__main__':
+	print(sum(
+		solve(list(map(list, block.splitlines())))
+		for block in sys.stdin.read().split('\n' * 2)
+	))
