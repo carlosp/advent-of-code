27f9835e427d06ac4fbd51fabd118c051d3ccdf3
Carlos Perez Alcaraz <alcacarl@amazon.com>
2023-12-14 22:04:38 +0100

    Add solution for puzzle 14 of 2023

---
+++ 2023/14-parabolic-reflector-dish/solve_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+CYCLE_TILT_DIRECTIONS = [(-1, 0)]
+NUM_CYCLES = 1
+
+def tilt(grid, height, width, direction, roundedRocks):
+	drow, dcol = direction
+	newRoundedRocks = []
+
+	for row, col in roundedRocks:
+		startRow, startCol = row, col
+		movedDistance = 0
+
+		while True:
+			row, col = row + drow, col + dcol
+
+			if row < 0 or row == height or col < 0 or col == width or grid[row][col] == '#':
+				break
+
+			movedDistance += (row, col) not in roundedRocks
+
+		newRoundedRocks += [(startRow + drow * movedDistance, startCol + dcol * movedDistance)]
+
+	return frozenset(newRoundedRocks)
+
+def computeTotalLoad(grid):
+	height, width = len(grid), len(grid[0])
+	roundedRocks = frozenset([(row, col) for row in range(height) for col in range(width) if grid[row][col] == 'O'])
+	seenStates = {}
+
+	for cycle in range(NUM_CYCLES):
+		seenStates[roundedRocks] = cycle
+
+		for direction in CYCLE_TILT_DIRECTIONS:
+			roundedRocks = tilt(grid, height, width, direction, roundedRocks)
+
+		if previousCycle := seenStates.get(roundedRocks):
+			cycleLength = (cycle + 1) - previousCycle
+			remainingCycles = (NUM_CYCLES - cycle - 1) % cycleLength
+			roundedRocks = next(state for state, cycle in seenStates.items() if cycle == previousCycle + remainingCycles)
+			break
+
+	return sum(height - row for row, _ in roundedRocks)
+
+
+if __name__ == '__main__':
+	grid = [list(line.strip()) for line in sys.stdin.readlines()]
+
+	print(computeTotalLoad(grid))
