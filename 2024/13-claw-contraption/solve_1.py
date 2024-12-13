69246655d0a9269320883855539ee1218547b070
Carlos Perez Alcaraz <alcacarl@amazon.com>
2024-12-13 17:15:48 +0100

    Add solution for puzzle 13 of 2024

---
+++ 2024/13-claw-contraption/solve_1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+import sys
+from itertools import starmap
+
+UNIT_CONVERSION_ERROR = 0
+
+def calculateMinTokensToWinAllPrizes(machines: list[tuple[int]]) -> int:
+	def calculateMinTokensToWin(ax: int, ay: int, bx: int, by: int, px: int, py: int) -> int:
+		px += UNIT_CONVERSION_ERROR
+		py += UNIT_CONVERSION_ERROR
+		b = (ax * py - ay * px) // (ax * by - ay * bx)
+		a = (px - b * bx) // ax
+
+		return 3 * a + b if a * ax + b * bx == px and a * ay + b * by == py else 0
+
+
+	return sum(starmap(calculateMinTokensToWin, machines))
+
+
+if __name__ == '__main__':
+	machines = [tuple(map(int, re.compile(r'\d+').findall(machine))) for machine in sys.stdin.read().split('\n\n')]
+
+	print(calculateMinTokensToWinAllPrizes(machines))
